12d2747cff3d86049e4abfd54e2aa2ca3b4049d3
abdulhaqani <ahaqa011@uottawa.ca>
2021-11-15 01:56:37 -0500

    comemyway and benchmark

--- src/components/BasicSlider.js
+++ src/components/BasicSlider.js
@@ -5,6 +5,8 @@ import antagonist from '../public/antagonist-blurred.png';
 import elevatorMsc from '../public/elevator.msc.png';
 import nightingale from '../public/nightingale.jpg';
 import tesla from '../public/tesla.png';
+import comemyway from '../public/comemyway.jpg';
+import benchmark from '../public/benchmark.jpg';
 import '../scss/home.scss';
 import '../scss/default.scss';
 
@@ -19,14 +21,23 @@ const BasicSlider = () => {
 
   return (
     <Slider {...settings}>
-      <div className="antagonist-container">
-        <div className="antagonist"></div>
-        <img src={antagonist} className="new-releases-image" alt="antagonist" />
-      </div>
       <div className="baraku-container">
         <div className="baraku"></div>
         <img src={baraku} className="new-releases-image" alt="baraku" />
       </div>
+      <div className="comemyway-container">
+        <div className="comemyway"></div>
+        <img src={comemyway} className="new-releases-image" alt="comemyway" />
+      </div>
+      <div className="benchmark-container">
+        <div className="benchmark"></div>
+        <img src={benchmark} className="new-releases-image" alt="benchmark" />
+      </div>
+      <div className="antagonist-container">
+        <div className="antagonist"></div>
+        <img src={antagonist} className="new-releases-image" alt="antagonist" />
+      </div>
+
       <div className="tesla-container">
         <div className="tesla"></div>
         <img src={tesla} className="new-releases-image" alt="tesla" />
