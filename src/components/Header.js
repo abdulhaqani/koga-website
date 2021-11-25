c62510e5224c448794466682a9c3fb9b88bd1d94
abdulhaqani <ahaqa011@uottawa.ca>
2021-11-25 02:23:25 -0500

    nav styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,98 @@
 import React from 'react';
 import '../scss/header.scss';
 import '../scss/home.scss';
+import { Link } from 'react-router-dom';
+
 // import ButtonTest from './ButtonTest';
 // import { Container } from 'semantic-ui-react';
 
 const Header = () => {
   return (
-    <div className="ui grid nav-links">
-      <div className="six wide column center aligned nav-link">Brand</div>
-      <div className="two wide column center aligned nav-link">Artists</div>
-      <div className="two wide column center aligned nav-link">Music</div>
-      <div className="two wide column center aligned nav-link">Contact</div>
-      <div className="two wide column center aligned nav-link">About</div>
-      <div className="two wide column center aligned nav-link">Race Club</div>
-    </div>
+    <ul className="ui grid nav-links">
+      <ul className="five wide column ui grid icons">
+        <li className="two wide column"></li>
+        <li className="nav-link three wide column">
+          <a
+            className="nav-link"
+            href="https://github.com/abdulhaqani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fas fa-home"></i>
+          </a>
+        </li>
+        <li className="nav-link three wide column">
+          <a
+            className="nav-link"
+            href="https://github.com/abdulhaqani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+        <li className="nav-link three wide column">
+          <a
+            className="nav-link"
+            href="https://github.com/abdulhaqani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-facebook-square"></i>
+          </a>
+        </li>
+        <li className="nav-link three wide column">
+          <a
+            className="nav-link"
+            href="https://github.com/abdulhaqani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-youtube"></i>
+          </a>
+        </li>
+        <li className="nav-link two wide column">
+          <a
+            className="nav-link"
+            href="https://github.com/abdulhaqani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-spotify"></i>
+          </a>
+        </li>
+      </ul>
+
+      <li className="two wide column"></li>
+      <ul className="nine wide column ui grid">
+        <li className="three wide column center aligned nav-link">
+          <Link to={'/artists'} className="nav-link">
+            Artists
+          </Link>
+        </li>
+        <li className="three wide column center aligned nav-link">
+          <Link to={'/music'} className="nav-link">
+            Music
+          </Link>
+        </li>
+        <li className="three wide column center aligned nav-link">
+          <Link to={'/contact'} className="nav-link">
+            Contact
+          </Link>
+        </li>
+        <li className="three wide column center aligned nav-link">
+          <Link to={'/about'} className="nav-link">
+            About
+          </Link>
+        </li>
+        <li className="three wide column center aligned nav-link">
+          <Link to={'/raceclub'} className="nav-link">
+            Raceclub
+          </Link>
+        </li>
+        <li className="one wide column center aligned nav-link"></li>
+      </ul>
+    </ul>
   );
 };
 
