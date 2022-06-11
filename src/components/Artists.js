a89f1f1f43e35f7d5cd8b07b53e9c0f2694c7ea3
abdulhaqani <ahaqa011@uottawa.ca>
2022-06-11 02:27:38 -0400

    artists links updated

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 import antagonist from '../public/antagonist-blurred.png';
 import '../scss/artists.scss';
 
@@ -12,16 +13,24 @@ const Artists = () => {
       </div>
       <div className="artists">
         <div className="artist center">
-          <img src={antagonist} className="artist-image" alt="antagonist" />
+          <Link to={'/artists/trainingseason'}>
+            <img src={antagonist} className="artist-image" alt="antagonist" />
+          </Link>
         </div>
         <div className="artist center">
-          <img src={antagonist} className="artist-image" alt="antagonist" />
+          <Link to={'/artists/sonrob'}>
+            <img src={antagonist} className="artist-image" alt="antagonist" />
+          </Link>
         </div>
         <div className="artist center">
-          <img src={antagonist} className="artist-image" alt="antagonist" />
+          <Link to={'/artists/greenbeard'}>
+            <img src={antagonist} className="artist-image" alt="antagonist" />
+          </Link>
         </div>
         <div className="artist center">
-          <img src={antagonist} className="artist-image" alt="antagonist" />
+          <Link to={'/artists/goodwinter'}>
+            <img src={antagonist} className="artist-image" alt="antagonist" />
+          </Link>
         </div>
       </div>
       <Footer />
