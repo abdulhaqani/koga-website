d13af2f1c0a30e553ee864fae6f7094db9d701a8
abdulhaqani <ahaqa011@uottawa.ca>
2022-06-14 02:45:50 -0400

    artists modals added

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import { Modal, Image, Button, Icon } from 'semantic-ui-react';
+import { TransitionModal } from 'semantic-ui-react-transition-modal';
 import antagonist from '../public/antagonist-blurred.png';
 import '../scss/artists.scss';
 
 const Artists = () => {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <div className="artists-wrap">
       <div className="navbar">
@@ -13,24 +17,80 @@ const Artists = () => {
       </div>
       <div className="artists">
         <div className="artist center">
-          <Link to={'/artists/trainingseason'}>
-            <img src={antagonist} className="artist-image" alt="antagonist" />
-          </Link>
+          <TransitionModal
+            animation="scale"
+            duration={300}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={
+              <img
+                src={antagonist}
+                className="artist-image modal"
+                alt="antagonist"
+              />
+            }
+          >
+            <Modal.Header>Profile Picture</Modal.Header>
+            <Modal.Content image scrolling>
+              <Image
+                size="medium"
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+                wrapped
+              />
+
+              <Modal.Description>
+                <p>
+                  This is an example of expanded content that will cause the
+                  modal's dimmer to scroll.
+                </p>
+
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image
+                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                  style={{ marginBottom: 10 }}
+                />
+                <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => setOpen(false)} primary>
+                Proceed <Icon name="chevron right" />
+              </Button>
+            </Modal.Actions>
+          </TransitionModal>
         </div>
         <div className="artist center">
-          <Link to={'/artists/sonrob'}>
-            <img src={antagonist} className="artist-image" alt="antagonist" />
-          </Link>
+          <img src={antagonist} className="artist-image" alt="antagonist" />
         </div>
         <div className="artist center">
-          <Link to={'/artists/greenbeard'}>
-            <img src={antagonist} className="artist-image" alt="antagonist" />
-          </Link>
+          <img src={antagonist} className="artist-image" alt="antagonist" />
         </div>
         <div className="artist center">
-          <Link to={'/artists/goodwinter'}>
-            <img src={antagonist} className="artist-image" alt="antagonist" />
-          </Link>
+          <img src={antagonist} className="artist-image" alt="antagonist" />
         </div>
       </div>
       <Footer />
