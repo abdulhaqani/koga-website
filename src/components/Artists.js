a83062cd898c9115d71c66bb0b95ad347c9d96f0
abdulhaqani <ahaqa011@uottawa.ca>
2022-06-15 02:07:14 -0400

    artist modal styling

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -31,56 +31,58 @@ const Artists = () => {
               />
             }
           >
-            <Modal.Header>Profile Picture</Modal.Header>
-            <Modal.Content image scrolling>
-              <Image
-                size="medium"
-                src="https://react.semantic-ui.com/images/wireframe/image.png"
-                wrapped
-              />
+            <div className="artistModal">
+              <Modal.Header>Profile Picture</Modal.Header>
+              <Modal.Content image scrolling>
+                <Image
+                  size="medium"
+                  src="https://react.semantic-ui.com/images/wireframe/image.png"
+                  wrapped
+                />
 
-              <Modal.Description>
-                <p>
-                  This is an example of expanded content that will cause the
-                  modal's dimmer to scroll.
-                </p>
+                <Modal.Description>
+                  <p>
+                    This is an example of expanded content that will cause the
+                    modal's dimmer to scroll.
+                  </p>
 
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image
-                  src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
-                  style={{ marginBottom: 10 }}
-                />
-                <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-              </Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-              <Button onClick={() => setOpen(false)} primary>
-                Proceed <Icon name="chevron right" />
-              </Button>
-            </Modal.Actions>
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+                    style={{ marginBottom: 10 }}
+                  />
+                  <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+                </Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button onClick={() => setOpen(false)} primary>
+                  Proceed <Icon name="chevron right" />
+                </Button>
+              </Modal.Actions>
+            </div>
           </TransitionModal>
         </div>
         <div className="artist center">
