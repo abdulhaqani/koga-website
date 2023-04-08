40ebc18dcc3d3d9496ce3c39ed34c63fae5509d1
abdulhaqani <ahaqa011@uottawa.ca>
2023-04-08 18:47:15 -0400

    fixed responsive dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu, Dropdown } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+
 import '../scss/header.scss';
 import '../scss/home.scss';
 import '../scss/default.scss';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleScreenSizeChange = () => {
+    setIsMobile(window.innerWidth < 800);
+  };
+
+  // listen for screen size changes
+  window.addEventListener('resize', handleScreenSizeChange);
+
+  // remove the event listener when the component unmounts
+  React.useEffect(() => {
+    handleScreenSizeChange();
+    return () => window.removeEventListener('resize', handleScreenSizeChange);
+  }, []);
+
   return (
     <div className="navbar">
       <div className="nav-container">
         <ul className="ui grid nav-links">
-          <ul className="five wide column ui grid icons">
-            <li className="two wide column"></li>
-
+          <ul className="seven wide column ui grid icons">
             <li className="nav-link three wide column">
               <Link to={'/'} className="nav-link">
                 <i class="fas fa-home"></i>
@@ -59,40 +75,81 @@ const Header = () => {
             </li>
           </ul>
 
-          <li className="two wide column"></li>
-          <ul className="nine wide column ui grid nav-container">
-            <li className="three wide column center aligned nav-link">
-              <Link to={'/artists'} className="nav-link">
-                Artists
-              </Link>
-            </li>
-            <li className="three wide column center aligned nav-link">
-              <Link to={'/Videos'} className="nav-link">
-                Videos
-              </Link>
-            </li>
-
-            <li className="three wide column center aligned nav-link">
-              <Link to={'/about'} className="nav-link">
-                About
-              </Link>
-            </li>
-            <li className="three wide column center aligned nav-link">
-              <Link to={'/contact'} className="nav-link">
-                Contact
-              </Link>
-            </li>
-            <li className="three wide column center aligned nav-link">
-              <Link to={'/merch'} className="nav-link">
-                Merch
-              </Link>
-            </li>
-            <li className="one wide column center aligned nav-link"></li>
-          </ul>
+          <li className="one wide column"></li>
+          {isMobile ? (
+            <div className="eight wide column">
+              <Dropdown
+                item
+                icon="bars"
+                floating
+                pointing
+                className="top right mobile-menu-dropdown"
+              >
+                <Dropdown.Menu>
+                  <Dropdown.Item className="three wide column center aligned nav-link">
+                    <Link to={'/artists'} className="nav-link">
+                      Artists
+                    </Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item className="three wide column center aligned nav-link">
+                    <Link to={'/Videos'} className="nav-link">
+                      Videos
+                    </Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item className="three wide column center aligned nav-link">
+                    <Link to={'/contact'} className="nav-link">
+                      Contact
+                    </Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item className="three wide column center aligned nav-link">
+                    <Link to={'/about'} className="nav-link">
+                      About
+                    </Link>
+                  </Dropdown.Item>
+                  <Dropdown.Item className="three wide column center aligned nav-link">
+                    <Link to={'/merch'} className="nav-link">
+                      Merch
+                    </Link>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          ) : (
+            <ul className="eight wide column ui grid nav-container">
+              <li className="three wide column center aligned nav-link">
+                <Link to={'/artists'} className="nav-link">
+                  Artists
+                </Link>
+              </li>
+              <li className="three wide column center aligned nav-link">
+                <Link to={'/Videos'} className="nav-link">
+                  Videos
+                </Link>
+              </li>
+              <li className="three wide column center aligned nav-link">
+                <Link to={'/about'} className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li className="three wide column center aligned nav-link">
+                <Link to={'/merch'} className="nav-link">
+                  Store
+                </Link>
+              </li>
+              <li className="three wide column center aligned nav-link">
+                <Link to={'/contact'} className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          )}
         </ul>
       </div>
     </div>
   );
 };
-
 export default Header;
+
+{
+  /*  */
+}
